048a7ac56205b9bc2b581178c61cfee886c77e73
sahiljain <sahil.jain@live.ca>
2013-08-30 01:53:16 -0400

    mostly done

--- EightQueensPuzzle/src/mainclass.java
+++ EightQueensPuzzle/src/mainclass.java
@@ -1,5 +1,6 @@
 public class mainclass {
 
+	static int solutions = 0;
 	public static void main(String[] args) {
 		// the strategy is to have an int array with 8 numbers in it,
 		// representing the row number of that specific pawn
@@ -7,12 +8,17 @@ public class mainclass {
 		for (int i = 0; i < pawns.length; i++) {
 			pawns[i] = -1;
 		}
-		pawns[0]=0;
-		//outputBoard(pawns);
+		pawns[0] = 0;
+		// outputBoard(pawns);
 		checkIfValid(pawns, 0);
 
 	}
 
+	/**
+	 * 
+	 * @param pawns
+	 * @param curPawn
+	 */
 	public static void checkIfValid(int[] pawns, int curPawn) {
 		// this method will check if the current arrangement is valid, by making
 		// sure current pawn is not
@@ -22,13 +28,20 @@ public class mainclass {
 		// it will delete, previous pawn will increment by 1.
 		// if column reaches 8 will output
 		// boolean isValid = false;
-		System.out.println("starting");
+		//System.out.println("starting");
 
+		
+		//if curPawn is on the board
+		if(pawns[curPawn] < 8){
 		if (horizontalValid(pawns, curPawn) && diagonalValid(pawns, curPawn)) {
 			//System.out.println("all valid");
 			// valid board and filled
 			if (curPawn == 7) {
 				outputBoard(pawns);
+				solutions++;
+				System.out.println(solutions);
+				pawns[curPawn]++;
+				checkIfValid(pawns, curPawn);
 			} else {
 				// valid and unfilled
 				curPawn++;
@@ -38,21 +51,28 @@ public class mainclass {
 
 		} else {
 			// invalid placed rightmost pawn, edit position
-			System.out.println("invalid");
+			//System.out.println("invalid");
 		//	outputBoard(pawns);
 			 pawns[curPawn]++;
-			int checkme = pawns[curPawn];
-			outputBoard(pawns);
+			//int checkme = pawns[curPawn];
+			//outputBoard(pawns);
 			//System.out.println(checkme +" is checkme");
 			// see if you went off the board
-			if (checkme == 8) {
-				pawns[curPawn] = -1;
-				curPawn--;
-				pawns[curPawn]++;
-				
-			}
+//			if (checkme > 7) {
+//				pawns[curPawn] = -1;
+//				curPawn--;
+//				pawns[curPawn]++;
+//				
+//			}
 			checkIfValid(pawns, curPawn);
 
+		}
+		}else{
+			//curPawn is past board
+			pawns[curPawn] = -1;
+			curPawn--;
+			pawns[curPawn]++;
+			checkIfValid(pawns,curPawn);
 		}
 
 	}
@@ -62,20 +82,23 @@ public class mainclass {
 		for (int i = 0; i < pawns.length; i++) {
 			// iterate through columns
 			int pawnRow = pawns[i];
-			if(pawnRow!=-1){
-			for (int j = 0; j < pawnRow; j++) {
-				System.out.print("_ ");
-			}
-			System.out.print("Q ");
-			for (int j = pawnRow + 1; j < 8; j++) {
-				System.out.print("_ ");
-			}
-			}else{
+			if (pawnRow != -1) {
+				// System.out.println(pawnRow);
+				for (int j = 0; j < pawnRow; j++) {
+					System.out.print("_ ");
+				}
+				System.out.print("Q ");
+				for (int j = pawnRow + 1; j < 8; j++) {
+					System.out.print("_ ");
+				}
+			} else {
 				System.out.print("_ _ _ _ _ _ _ _ ");
+
 			}
 			System.out.print("\n");
 
 		}
+		System.out.println();
 	}
 
 	static boolean horizontalValid(int[] pawns, int curPawn) {
@@ -86,9 +109,9 @@ public class mainclass {
 			if (pawns[i] == curRow)
 				return false;
 		}
-		System.out.println("horiz valid");
+		//System.out.println("horiz valid");
 		return true;
-		
+
 	}
 
 	static boolean diagonalValid(int[] pawns, int curPawn) {
